efda926842e19a34175e298ae5d77179c59964d9
Eduard Smetanin <eduard.smetanin@gmail.com>
2020-04-20 15:42:49 -0600

    Added general noise gate and specific amplitude damping noise gate. Fixed build function that was inserting transposed (by mistake) gate into full register gate. Removed no longer necessary first_set_bit_position function. Renamed QuantumRegister to Program and delayed applying gates until new exec function gets called (to accommodate probabilistic applying of Kraus gates).

--- src/FullRegisterGate.jl
+++ src/FullRegisterGate.jl
@@ -2,39 +2,36 @@ module FullRegisterGate
 
 # build expands the given gate with optional control qubits to entire quantum register with the given size.
 function build(register_size::Integer,
-               gate::AbstractMatrix{Complex{Float64}},
-               gate_lowest_index::Integer,
-               control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([])
-              )::AbstractMatrix{Complex{Float64}}
+	gate::AbstractMatrix{Complex{Float64}},
+	gate_lowest_index::Integer,
+	control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([])
+	)::AbstractMatrix{Complex{Float64}}
+
 	small_gate_size = size(gate)[1]
 	small_gate_qubit_count = Int(log2(small_gate_size))
-	small_gate_bitmask = gate_bitmask(small_gate_qubit_count, gate_lowest_index)
-	if small_gate_bitmask == 0
-		error("small_gate_bitmask cannot be 0")
-	end
-	small_gate_offset = first_set_bit_position(small_gate_bitmask)
-	if small_gate_offset == -1
-		error("internal error: small_gate_offset should not be -1")
+	gate_bitmask = gate_full_register_bitmask(small_gate_qubit_count, gate_lowest_index)
+	if gate_bitmask == 0
+		error("gate_bitmask cannot be 0")
 	end
 	n = 2 ^ register_size
 	big_gate = zeros(Complex{Float64}, n, n)
-	ctrl_bitmask = control_bitmask(control_bit_indexes)
-	for big_gate_row_index ∈ 0:n-1
-		if !all_control_bits_set(big_gate_row_index, ctrl_bitmask)
-			big_gate[big_gate_row_index+1, big_gate_row_index+1] = Complex(1)
+	control_bitmask = control_full_register_bitmask(control_bit_indexes)
+	for big_gate_column_index ∈ 0:n-1
+		if !all_control_bits_set(big_gate_column_index, control_bitmask)
+			big_gate[big_gate_column_index+1, big_gate_column_index+1] = Complex(1)
 			continue
 		end
-		target_bits = (big_gate_row_index & small_gate_bitmask) >>> small_gate_offset
-		output_state = gate[:,target_bits+1]
+		target_bits = (big_gate_column_index & gate_bitmask) >>> gate_lowest_index
+		output_state = gate[:,target_bits+1] # Selecting a column here yields the result of matrix-vector multiplication.
 		for state_index ∈ 0:small_gate_size-1
-			big_gate_column_index = (big_gate_row_index & ~small_gate_bitmask) | (state_index << small_gate_offset)
+			big_gate_row_index = (big_gate_column_index & ~gate_bitmask) | (state_index << gate_lowest_index)
 			big_gate[big_gate_row_index+1, big_gate_column_index+1] = Complex(output_state[state_index+1])
 		end
 	end
 	return big_gate
 end
 
-function gate_bitmask(gate_qubit_count::Integer, gate_lowest_index::Integer)::UInt64
+function gate_full_register_bitmask(gate_qubit_count::Integer, gate_lowest_index::Integer)::UInt64
 	bitmask = zero(UInt64)
 	for _ ∈ 1:gate_qubit_count
 		bitmask <<= 1
@@ -43,7 +40,7 @@ function gate_bitmask(gate_qubit_count::Integer, gate_lowest_index::Integer)::UI
 	return bitmask << gate_lowest_index
 end
 
-function control_bitmask(control_bit_indexes::AbstractArray{Int64,1})::UInt64
+function control_full_register_bitmask(control_bit_indexes::AbstractArray{Int64,1})::UInt64
 	bitmask = zero(UInt64)
 	for i ∈ control_bit_indexes
 		bitmask |= one(UInt64) << i
@@ -53,24 +50,6 @@ end
 
 @inline function all_control_bits_set(i::Integer, control_bitmask::Integer)
 	return i & control_bitmask == control_bitmask
-end
-
-# first_set_bit_position returns position of the first 1 in the binary
-# representation of the given integer counting from the right.
-# Returns -1 if 1 not found.
-function first_set_bit_position(i::Integer)::Integer
-	total_bits = sizeof(i) * 8
-	position = 0
-	while true
-		if i & 1 == 1
-			return position
-		end
-		if position == total_bits - 1
-			return -1
-		end
-		position += 1
-		i >>>= 1
-	end
 end
 
 end # module
--- src/NoisyQuantumComputerSimulator.jl
+++ src/NoisyQuantumComputerSimulator.jl
@@ -1,75 +1,157 @@
 module NoisyQuantumComputerSimulator
 
-export QuantumRegister, gate, h, x, y, z, swap, s
+using LinearAlgebra
+
+export Program, exec, gate, h, x, y, z, swap, s, noise, amplitude_damping
 
 include("FullRegisterGate.jl")
 
-mutable struct QuantumRegister
+mutable struct Program
 	qubit_count:: Int
 	density_matrix::Array{Complex{Float64}}
+	commands::Array{Function}
 
-	# QuantumRegister creates quantum register with the given size and sets it's value to |0⟩
-	function QuantumRegister(size::Integer)
+	# Program creates quantum register with the given size and sets it's value to |0⟩.
+	function Program(size::Integer)
 		state_size = 2^size
 		density_matrix = zeros(Complex{Float64}, state_size, state_size)
 		density_matrix[1,1] = 1
-		return new(Int(size), density_matrix)
+		return new(Int(size), density_matrix, Array{Function}[])
 	end
 end
 
-# gate runs the given gate on the given quantum register with optional control bits.
-function gate(register::QuantumRegister, 
-              matrix::AbstractMatrix{Complex{Float64}},
-              gate_lowest_index::Integer,
-	      control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]) # TODO: Can it be Integer?
-	     )
-	big_matrix = FullRegisterGate.build(register.qubit_count, matrix, gate_lowest_index, control_bit_indexes)
-	register.density_matrix = big_matrix * register.density_matrix * big_matrix'
+function exec(program::Program)
+	for command ∈ program.commands
+		command()
+	end
 	return
 end
 
-# gate runs Hadamard gate on the given quantum register with optional control bits.
-function h(register::QuantumRegister, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
-	gate(register, Matrix{Complex{Float64}}(1/√2 * [1 1; 1 -1]), index, control_bit_indexes)
+# gate runs the given gate with optional control bits.
+function gate(program::Program, 
+	matrix::AbstractMatrix{Complex{Float64}},
+	gate_lowest_index::Integer,
+	control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]) # TODO: Can it be Integer?
+	)
+	push!(program.commands, ()->begin
+		big_matrix = FullRegisterGate.build(program.qubit_count, matrix, gate_lowest_index, control_bit_indexes)
+		program.density_matrix = big_matrix * program.density_matrix * big_matrix'
+		return
+	end)
 	return
 end
 
-# gate runs X gate on the given quantum register with optional control bits.
-function x(register::QuantumRegister, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
-	gate(register, Matrix{Complex{Float64}}([0 1; 1 0]), index, control_bit_indexes)
+# h runs Hadamard gate with optional control bits.
+function h(program::Program, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+	gate(program, Matrix{Complex{Float64}}(1/√2 * [1 1; 1 -1]), index, control_bit_indexes)
 	return
 end
 
-# gate runs Y gate on the given quantum register with optional control bits.
-function y(register::QuantumRegister, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
-	gate(register, Matrix{Complex{Float64}}([0 complex(0, -1); complex(0, 1) 0]), index, control_bit_indexes)
+# x runs X gate with optional control bits.
+function x(program::Program, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+	gate(program, Matrix{Complex{Float64}}([0 1; 1 0]), index, control_bit_indexes)
 	return
 end
 
-# gate runs Z gate on the given quantum register with optional control bits.
-function z(register::QuantumRegister, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
-	gate(register, Matrix{Complex{Float64}}([1 0; 0 -1]), index, control_bit_indexes)
+# y runs Y gate with optional control bits.
+function y(program::Program, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+	gate(program, Matrix{Complex{Float64}}([0 complex(0, -1); complex(0, 1) 0]), index, control_bit_indexes)
 	return
 end
 
-# gate runs SWAP gate on the given quantum register with optional control bits.
-function swap(register::QuantumRegister,
-              index_a::Integer,
-              index_b::Integer,
-              control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([])
-             )
+# z runs Z gate with optional control bits.
+function z(program::Program, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+	gate(program, Matrix{Complex{Float64}}([1 0; 0 -1]), index, control_bit_indexes)
+	return
+end
+
+# swap runs SWAP gate with optional control bits.
+function swap(program::Program,
+	index_a::Integer,
+	index_b::Integer,
+	control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+
 	controls_a = vcat(index_a, control_bit_indexes)
 	controls_b = vcat(index_b, control_bit_indexes)
-	x(register, index_a, controls_b)
-	x(register, index_b, controls_a)
-	x(register, index_a, controls_b)
+	x(program, index_a, controls_b)
+	x(program, index_b, controls_a)
+	x(program, index_a, controls_b)
 	return
 end
 
-# gate runs phase gate (S) on the given quantum register with optional control bits.
-function s(register::QuantumRegister, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
-	gate(register, Matrix{Complex{Float64}}([1 0; 0 complex(0,1)]), index, control_bit_indexes)
+# s runs phase gate (S) with optional control bits.
+function s(program::Program, index::Integer, control_bit_indexes::AbstractArray{Int64,1} = Array{Int64,1}([]))
+	gate(program, Matrix{Complex{Float64}}([1 0; 0 complex(0,1)]), index, control_bit_indexes)
 	return
+end
+
+# noise runs the specified via Kraus operators noise gate.
+function noise(program::Program, 
+	kraus_operators::AbstractVector{Matrix{Complex{Float64}}},
+	gate_lowest_index::Integer)
+
+	# println("------------------------------------------------------------------------------------")
+	# println("kraus_operators: $kraus_operators")
+	push!(program.commands, ()->begin
+		kraus_probabilities = Vector{Float64}([])
+		big_kraus_operators = Vector{Matrix{Complex{Float64}}}([])
+		for kraus ∈ kraus_operators
+			big_kraus = FullRegisterGate.build(program.qubit_count, kraus, gate_lowest_index)
+			# println("big_kraus: $big_kraus")
+			trace = tr(big_kraus * program.density_matrix * big_kraus')
+			if imag(trace) ≉ 0
+				error("expected trace be a real value since it's probability, got $trace")
+			end
+			kraus_probability = real(tr(big_kraus * program.density_matrix * big_kraus'))
+			# println("kraus_probability: $kraus_probability")
+			push!(big_kraus_operators, big_kraus)
+			push!(kraus_probabilities, kraus_probability)
+		end
+		random_kraus_index = random_index(kraus_probabilities)
+		# println("random_kraus_index: $random_kraus_index")
+		random_big_kraus = big_kraus_operators[random_kraus_index]
+		# println("random_big_kraus: $random_big_kraus")
+		random_big_kraus_probability = kraus_probabilities[random_kraus_index]
+		# println("random_big_kraus_probability: $random_big_kraus_probability")
+		program.density_matrix = random_big_kraus * program.density_matrix * random_big_kraus' / random_big_kraus_probability
+		# println("program.density_matrix: $(program.density_matrix)")
+		return
+	end)
+	return
+end
+
+damping_residual_kraus(decay_1_to_0_probability = .1) = Matrix{Complex{Float64}}([1 0; 0 √(1-decay_1_to_0_probability)])
+damping_kraus(decay_1_to_0_probability = .1) = Matrix{Complex{Float64}}([0 √decay_1_to_0_probability; 0 0])
+damping_kraus_operators(decay_1_to_0_probability = .1) = [damping_residual_kraus(decay_1_to_0_probability), damping_kraus(decay_1_to_0_probability)]
+
+# amplitude_damping adds noise via amplitude damping kraus operators with given probability of decay |1> to |0>.
+function amplitude_damping(program::Program, 
+	ket1_to_ket0_decay_probability::Real,
+	gate_lowest_index::Integer)
+
+	noise(program, damping_kraus_operators(ket1_to_ket0_decay_probability), gate_lowest_index)
+	return
+end
+
+# random_index returns random index according to the given probabilities.
+# Min returned value is 1; max return value is the length of the given array of probabilities.
+function random_index(probabilities::AbstractVector{Float64})
+	random = rand()
+	# println("random: $random")
+	return value_index_by_probabilities(random, probabilities)
+end
+
+# value_index_by_probabilities returns index of the given value according to the given probabilities.
+# Min returned value is 1; max return value is the length of the given array of probabilities.
+function value_index_by_probabilities(value::Float64, probabilities::AbstractVector{Float64})
+	total_probability = zero(Float64)
+	for (index, probability) ∈ enumerate(probabilities)
+		total_probability += probability
+		if value < total_probability
+			return index
+		end
+	end
+	return length(probabilities) # Unlikely, but control might reach here in case of some rounding issues.
 end
 
 # function product!(state::AbstractArray, gate::AbstractMatrix{Complex{Float64}})
